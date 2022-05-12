1aee58d882aa4b6f89e6ddfc45826229cf1f372d
Uros Spasojevic <uspasojevic96@gmail.com>
2022-05-12 21:05:36 +0200

    initial commit

---
+++ src/graphql.ts
@@ -0,0 +1,14 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export interface IQuery {
+    hello(): Nullable<string> | Promise<Nullable<string>>;
+}
+
+type Nullable<T> = T | null;
